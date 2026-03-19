37d9ba080628338aa0ede7e4ed69fd76e8f6290c
Bartek K <bart3kgithub@gmail.com>
2026-03-19 07:55:49 +0100

    tests

---
+++ Bank/Bank.Tests/KontoTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bank;
+
+namespace Bank.Tests;
+
+[TestClass]
+public class KontoTests
+{
+    [TestMethod]
+    public void Konstruktor_Prawidlowe_Dane_Tworzy_Konto()
+    {
+        var konto = new Konto("Jan Kowalski", 100);
+
+        Assert.AreEqual("Jan Kowalski", konto.Nazwa);
+        Assert.AreEqual(100, konto.Bilans);
+        Assert.IsFalse(konto.Zablokowane);
+    }
+
+    
+}
