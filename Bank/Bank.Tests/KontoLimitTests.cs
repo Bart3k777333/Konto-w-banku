72180755324467f9a7b5e79b16155a297d7343fe
Bartek K <bart3kgithub@gmail.com>
2026-03-29 14:43:19 +0200

    Limit_Ustawienie_Ujemnego_Wyrzuca_Wyjatek

--- Bank/Bank.Tests/KontoLimitTests.cs
+++ Bank/Bank.Tests/KontoLimitTests.cs
@@ -7,5 +7,10 @@ namespace Bank.Tests;
 [TestClass]
 public class KontoLimitTests
 {
-
+    [TestMethod]
+    public void Limit_Ustawienie_Ujemnego_Wyrzuca_Wyjatek()
+    {
+        var konto = new KontoLimit("Jan Kowalski", 100, 100);
+        Assert.Throws<ArgumentOutOfRangeException>(() => konto.Limit = -10);
+    }
 }
